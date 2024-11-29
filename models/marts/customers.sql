f355b5d77afd2ce87b6a7d852210926dac426ecc
ekaterina.khrushch@contentful.com <ekaterina.khrushch@contentful.com>
2024-11-29 09:51:59 +0000

    pull

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,22 +1,25 @@
-with customer_data AS (
+with customer_data as (
 
-    select customer_id
-            , customer_name
-    from {{ref('stg_customers')}}
-)
+    select
+        customer_id,
+        customer_name
+    from {{ ref('stg_customers') }}
+),
 
-, orders AS (
-    
-    select customer_id
-            , count(distinct order_id) as total_order_count
-            , sum(order_total) as total_order_amount
-    from {{ref('stg_orders')}}
+orders as (
+
+    select
+        customer_id,
+        count(distinct order_id) as total_order_count,
+        sum(order_total) as total_order_amount
+    from {{ ref('stg_orders') }}
     group by 1
 )
 
-select customer_data.customer_id
-        , customer_name
-        , total_order_count
-        , total_order_amount
+select
+    customer_data.customer_id,
+    customer_name,
+    total_order_count,
+    total_order_amount
 from customer_data
 left join orders on customer_data.customer_id = orders.customer_id
